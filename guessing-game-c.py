7bcb0603e08c4bb3d8e714ef600967435898bf51
Nana <juniornyamile@gmail.com>
2022-01-11 20:28:20 +0000

    guessing game with thecomputer generating a secret number

---
+++ guessing-game-c.py
@@ -0,0 +1,14 @@
+import  random
+
+def guess(x):
+    random_number = random.randint(1, x) #this returns a random number from 1 to x, that we need to guess. where x is the value of range
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        print(guess)
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again, Too high.")
+    print(f"Hurray!!!!!, you entered the correct number which was <{random_number}>. Well Done.")
+guess(10)
--- guessing-game.py
+++
@@ -1,4 +0,0 @@
-'''in this game the computer pulls out a random number and 
-   the user has to guess what number it is'''
-
-import random
