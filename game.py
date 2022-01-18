52c1a5557f4ea64a7adbe6b61c3e612a7e304629
Nana <juniornyamile@gmail.com>
2022-01-18 22:54:44 +0000

    tictactoe modified

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
+from lib2to3.pytree import LeafPattern
+from re import T
+
+
 class TicTacToe:
     def __init__(self):
         self.board = [" " for _ in range(9)] # we will use a single list to rep 3x3 board
@@ -15,3 +19,42 @@ class TicTacToe:
         for row in number_board:
             print(" | " + " | ".join(row) + " | ")
 
+    def available_moves(self):
+        return [ i for i, spot in enumerate(self.board) if spot == " "]
+        
+        def empty_squares(self):
+            return " " in self.board
+
+        def num_empty_squares(self):
+            return len(self.available_moves())  # or return self.board.count(" ") to return the number of counts left. 
+        
+        def make_move(self, square, letter):
+            # if valid move, then make the move (assign square to letter)
+            # then return true. if invalid, return false
+            if self.board[square] == " ":
+                self.board[square] == letter 
+                return True
+            else:
+                return False
+
+        
+        # moves == []
+        # for (i, spot) in enumerate[self.board]:
+        #      # ['x', 'x', "o"] --> [(0, "x"), (1, 'x'), (2,"o")]
+        #      if spot == " ":
+        #            moves.append(i)
+        #return moves
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = "X" # for starting letter
+    # iterate while the game still has empty squares.
+    #we don'thave to worry about winners beacuse we'll just return that 
+    #which breaks the loop
+    while game.empty_squares():
+        # to get the move from the appropriate player
+        if  letter == "O":
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
--- player.py
+++ player.py
@@ -1,11 +1,10 @@
 import math
+from operator import truediv
 import random
 class Player:
     def __init__(self, letter):
         # letter is x or 0 
         self.letter = letter
-
-        
 
         #we want all players to get thier next move given a game
     def get_move(self, game):
@@ -16,11 +15,27 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
-
+        # get a random valid spot for our next move
+        square = random.chioice(game.available_moves())
+        return square
+        
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
-
+        
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "\"s turn. Input move(0-9):")
+            # we are going to check that this is a correct value by trying to cast 
+            # it to an integer, and if it's not, then we say it's invalid
+            #if that spot is not available on the board, we also say it's invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True # if these are succesful,then yay!
+            except ValueError:
+                print('invalid square. Try again.')
+        return val
