e979768b016e8b00fbdaf1a1a9213c080bae8a72
Nana <juniornyamile@gmail.com>
2022-02-06 21:26:56 +0000

    ds1

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 from player import GeniusComputerPlayer, HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 
-
 class TicTacToe:
     def __init__(self):
         self.board = [" " for _ in range(9)] # we will use a single list to rep 3x3 board
